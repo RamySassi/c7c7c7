8b44a5eb0761d048892b6c70998a5b140d83fb51
RamySassi <rami9b3@live.fr>
2024-02-16 00:12:58 +0100

    hello c7

--- src/Board.js
+++ src/Board.js
@@ -104,56 +104,136 @@ class Board extends EventEmitter {
 =                 TODO: fill in these Checker Functions                    =
 =========================================================================*/
 
-  rowSafe(row, num) {
-    //check if the row contains num
-  }
+  
+rowSafe(row, num) {
+  //check if the row contains num
+  let rows=this.getRow(row)
+// if(rows.includes(num)){
+//   return false
+// }
+// return true
+return !rows.includes(num)
+}
 
-  colSafe(col, num) {
-    //check if the column contains num
-  }
+colSafe(col, num) {
+  let cols=this.getCol(col)
+  //check if the column contains num
+  return !cols.includes(num)
+}
 
-  boxSafe(row, col, num) {
-    //check if the box contains num
-  }
+boxSafe(row, col, num) {
+  let box=this.getBox(row,col)
+  //check if the box contains num
+  return !box.includes(num)
+}
 
-  rowValidAt(rowIndex) {
-    //check if a row is valid at a given index
+rowValidAt(rowIndex) {
+  let row=this.getRow(rowIndex)
+  //check if a row is valid at a given index
+//  for(var i=0;i<9;i++){
+//   for(var j=i+1;j<9;j++){
+//     if(row[i]===row[j] && row[i]){
+//       return false
+//     }
+//   }
+//  }
+//  return true
+    
+for(var i=0;i<9;i++){
+  let element=row[i]
+  if(element&&row.lastIndexOf(element)!==i){
+    return false
   }
+}
+return true
+}
 
-  colValidAt(colIndex) {
-    //check if a column is valid at a given index
+colValidAt(colIndex) {
+  let col=this.getCol(colIndex)
+  for(var i=0;i<9;i++){
+    let element=col[i]
+    if(element&&col.lastIndexOf(element)!==i){
+      return false
+    }
   }
+  return true
+  //check if a column is valid at a given index
+}
 
-  boxValidAt(boxIndex) {
-    //check if a box is valid at a given index
+boxValidAt(boxIndex) {
+  let box=this.getBoxByIndex(boxIndex)
+  for(var i=0;i<9;i++){
+    let element=box[i]
+    if(element&&box.lastIndexOf(element)!==i){
+      return false
+    }
   }
+  return true
+  //check if a box is valid at a given index
+}
 
-  allRowsValid() {
-    //check if all the rows in the board are valid
+allRowsValid() {
+  for(var i=0;i<9;i++){
+    if(!this.rowValidAt(i)){
+      return false
+    }
   }
-  allColsValid() {
-    //check if all the columns in the board are valid
+  return true
+  //check if all the rows in the board are valid
+}
+allColsValid() 
+{
+  for(var i=0;i<9;i++){
+    if(!this.colValidAt(i)){
+      return false
+    }
   }
-  allBoxesValid() {
-    //check if all the boxes in the board are valid
+  return true
+  //check if all the columns in the board are valid
+}
+allBoxesValid() {
+  for(var i=0;i<9;i++){
+    if(!this.boxValidAt(i)){
+      return false
+    }
   }
+  return true
+  //check if all the boxes in the board are valid
+}
 
-  validBoard() {
-    return this.allBoxesValid() && this.allColsValid() && this.allRowsValid();
-  }
+validBoard() {
+  return this.allBoxesValid() && this.allColsValid() && this.allRowsValid();
+}
 
-  isSafe(row, col, num) {
-    return  this.rowSafe(row, num) && this.colSafe(col, num) && this.boxSafe(row, col, num);
-  }
-
+isSafe(row, col, num) {
+  return  this.rowSafe(row, num) && this.colSafe(col, num) && this.boxSafe(row, col, num);
+}
 /*=========================================================================
 =                 TODO: fill in these Solver Functions                    =
 =========================================================================*/
 
-  solve() {
-    //solve the board using a backtracking algorithm and return the solution
-  }
+solve() {
+    for (var row = 0; row < 9; row++) {
+    for (var col = 0; col < 9; col++) {
+      if (this.board[row][col] === 0) {
+    for (var num = 1; num <= 9; num++) {
+      if (this.isSafe(row, col, num)) {
+ this.board[row][col] = num;
+       if (this.solve()) {
+ return true}
+   this.board[row][col] = 0; 
+          }}
+        return false; 
+          }}}
+        return true; 
+          
+        
+      }
   
+
+
+
+
   solveBoard() {
     while (this.validBoard()) {
       if (this.solve()) {
@@ -163,6 +243,7 @@ class Board extends EventEmitter {
       return false
     }
     this.emit("invalidBoard");
+    return false
     // dont forget to add a small change here ;) 
   }
 }
